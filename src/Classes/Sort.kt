02648cc1c7a5a2e4969482b6fb3d1e0b69d156ef
AyMax77 <dehay.maxime77lvd@gmail.com>
2024-10-03 16:36:32 +0200

    Enregistrer a 16h36 3/10

--- src/Classes/Sort.kt
+++ src/Classes/Sort.kt
@@ -1,7 +1,33 @@
 package Classes
-
+import jeu.TirageDes
 import personnage.Personnage
+import item.Item
 
-class Sort(val nom:String,effet:(Personnage, Personnage ) -> Unit) {
+class Sort(val nom:String,var effet:(Personnage, Personnage ) -> Unit) {
 
+
+    val bouledefeu = Sort("Boule de Feu", { caster, cible ->
+        run {
+            val tirageDes = TirageDes(3, 6)
+            var degat = tirageDes.lance()
+            degat = maxOf(1, degat - cible.calculeDefense())
+            cible.pointDeVie -= degat
+            println("La boule de feu inflige $degat à ${cible.nom}")
+        }
+    })
+    val missilemagique = Sort("Missile magique", { caster, cible ->
+        run {
+            var compteur = 0
+            while(compteur <= 2){
+                val tirageDes = TirageDes(1, 6)
+                var degat = tirageDes.lance()
+                degat = maxOf(1, degat - cible.calculeDefense())
+                cible.pointDeVie -= degat
+                println("Les missiles magiques infligent $degat à ${cible.nom}")
+                compteur += 1
+            }
+
+        }
+
+    })
 }
--- src/jeu/Combat.kt
+++ src/jeu/Combat.kt
@@ -65,6 +65,7 @@ class Combat(
                         }
                     }
                 }
+
                 else -> {
                     println("Mauvaise saisie !!!")
                 }
